d3e21d5c6b0673c50b9b4ff7c42d308748605f49
Lucas Lacerda <111766399+Lacerda018@users.noreply.github.com>
2023-05-15 12:25:18 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,15 @@
+
+const botaoAlterarTema = document.getElementById('botao-alterar-tema');
+const imagemBotaoTrocaDeTema = document.querySelector('.imagem-botao')
+const body = document.querySelector("body")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains('modo-escuro')
+    if (modoEscuroEstaAtivo){
+        body.classList.remove('modo-escuro')
+        imagemBotaoTrocaDeTema.setAttribute('src', './imagens/sun.png')        
+    }else{
+        body.classList.add('modo-escuro');
+        imagemBotaoTrocaDeTema.setAttribute('src', './imagens/moon.png')
+    }
+});
