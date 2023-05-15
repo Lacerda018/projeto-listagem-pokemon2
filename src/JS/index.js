49216099b4039a74afc7a913c851682ef9ca01d4
Lucas Lacerda <111766399+Lacerda018@users.noreply.github.com>
2023-05-15 12:44:10 -0300

    Update index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -7,9 +7,9 @@ botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains('modo-escuro')
     if (modoEscuroEstaAtivo){
         body.classList.remove('modo-escuro')
-        imagemBotaoTrocaDeTema.setAttribute('src', './imagens/sun.png')        
+        imagemBotaoTrocaDeTema.setAttribute('src', './src/imagens/sun.png')        
     }else{
         body.classList.add('modo-escuro');
-        imagemBotaoTrocaDeTema.setAttribute('src', './imagens/moon.png')
+        imagemBotaoTrocaDeTema.setAttribute('src', './src/imagens/moon.png')
     }
 });
